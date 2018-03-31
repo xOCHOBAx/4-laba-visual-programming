4267febf6b62e27801bdad4e076d8333624d2caf
awfly <a-nton.99@yandex.ru>
2018-03-31 15:02:28 +0400

    Exit

--- 4 lab/4 lab/Form1.cs
+++ 4 lab/4 lab/Form1.cs
@@ -16,5 +16,10 @@ namespace _4_lab
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
